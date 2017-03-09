db2b16c80c7414c0381edc07792ac33c2f788c54
liuqingming <liumyua@163.com>
2017-03-09 15:45:28 +0800

    learn use timer and shutdown system

---
+++ learnTime.py
@@ -0,0 +1,12 @@
+﻿import threading
+from os import system
+from datetime import datetime
+
+def func():
+ print 'hi'
+ system('shutdown -r -t 6')
+ now = datetime.today()
+ print now.year,now.month,now.day,now.hours,now.minutes,now.seconds
+
+timer = threading.Timer(1,func)
+timer.start()
---
+++ showTime.py
@@ -0,0 +1,8 @@
+﻿from datetime import datetime
+from os import system
+import threading
+
+now = datetime.today()
+print now.year,now.month,now.day,now.hour,now.minute,now.second
+str="%s:%s:%s"%(now.hour,now.minute,now.second)
+print str
